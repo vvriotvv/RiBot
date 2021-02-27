13597a097b56066133b759b379cfb4786dca6c15
Chris <chris.freddy@hotmail.co.uk>
2021-02-27 21:41:44 +0000

    added node-fetch

---
+++ commands/fun/cat.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+module.exports = {
+	name: 'cat',
+	description: 'Random cat',
+	cooldown: 5,
+	async execute(message, args) {
+    	fetch('https://aws.random.cat/meow').then(response => response.json());
+		const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+
+		message.channel.send(file);
+	},
+};
